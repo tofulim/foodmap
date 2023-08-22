b5696cb0ef5ad7193ee78c6ab0ee8913d0972569
vail131 <vail131@naver.com>
2023-08-23 00:27:11 +0900

    [#1] add naver url crawl step

--- pattern_parser.py
+++ pattern_parser.py
@@ -1,9 +1,9 @@
 import re
 
 
-class Extractor:
+class Parser:
+    @staticmethod
     def get_match_pattern_string(
-        self,
         total_page: str,
         head_string: str,
         target_string: str,
@@ -11,5 +11,14 @@ class Extractor:
     ):
         pattern = f"(?<={head_string}).*?" + target_string + f"(?={tail_string})"
         total_pattern = re.findall(pattern, str(total_page))
+
+        return total_pattern
+
+    @staticmethod
+    def get_naver_url(
+        total_page: str,
+    ):
+        pattern = "https?://naver.me/[^-\\\]+"
+        total_pattern = re.search(pattern, total_page).group()
 
         return total_pattern
