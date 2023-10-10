8b10c7bb5a7a96ca690f8ba4affa8515816ed3a2
vail131 <vail131@naver.com>
2023-10-10 23:49:08 +0900

    [#5] init un-resolved

--- crawlers/parsers/base_parser.py
+++ crawlers/parsers/base_parser.py
@@ -1,7 +1,7 @@
 import re
 from abc import ABC, abstractmethod
 
-import requests
+from bs4 import BeautifulSoup
 
 
 class AbstractParser(ABC):
@@ -33,6 +33,23 @@ class AbstractParser(ABC):
 
         return total_pattern
 
+    @staticmethod
+    def get_thumbnail(html_source: BeautifulSoup) -> str:
+        """
+        bs4 html resource를 받아 문자열 썸네일을 반환한다.
+
+        Args:
+            html_source (BeautifulSoup):
+
+        Returns:
+            thumbnail_url: str
+
+        """
+
+        thumbnail = html_source.find("link", {"itemprop": "thumbnailUrl"})["href"]
+
+        return thumbnail
+
 
 class NaverMapParser(AbstractParser):
     """
@@ -40,56 +57,67 @@ class NaverMapParser(AbstractParser):
     """
 
     def __init__(self):
-        self.naver_url_shop_title_pattern = "data-line-title"
-        self.naver_url_shop_location_pattern = "data-line-description"
+        # 더보기란에서 정보를 얻겠다는 것
+        self.type = "more_info"
+        self.request_url = "https://www.youtube.com/watch?v={video_id}"
 
-    def parse(self, target_video_source: str, **kwargs):
-        naver_url = self.get_naver_url(
-            total_page=target_video_source,
-        )
-        video_id = self.get_match_pattern_string(
-            head_string=r'videoId"\:"',
-            target_string="",
-            tail_string=r'"\,"',
-            total_page=target_video_source,
-        )
-        title = self.get_match_pattern_string(
-            head_string=r'text"\:"',
-            target_string="",
-            tail_string=r'"\}\]',
-            total_page=target_video_source,
-        )
-        thumbnail_list = self.get_match_pattern_string(
-            head_string=r'url"\:"',
-            target_string="",
-            tail_string=r'"\,"',
-            total_page=target_video_source,
-        )
-        naver_map_source = requests.get(naver_url, verify=False)
-        # ISO-8859-1 인코딩 문제로 한글 깨지는 문제 해결
-        naver_map_source.encoding = "utf-8"
-        naver_map_source = naver_map_source.text
-        store_info = self.get_store_info(naver_map_source=naver_map_source)
+    # TODO: scraper로 분리해서 thumnail이랑 더보기란이나 댓글을 key:value로 구분해서 key: html_Source로 하게 해야함
+    def parse(self, video_id: str, **kwargs):
 
-        return {
-            "video_id": video_id[0],
-            "playlist_title": title[0],
-            "thumbnail_list": thumbnail_list,
-            "naver_url": naver_url,
-            **store_info,
-        }
+        # response = requests.get(
+        #     url=self.request_url.format(video_id=video_id), verify=False
+        # )
+        # html_source = BeautifulSoup(response.text, "html.parser")
+        #
+        # html_source.find()
+        # naver_url = self.get_naver_url(
+        #     total_page=target_video_source,
+        # )
+        # video_id = self.get_match_pattern_string(
+        #     head_string=r'videoId"\:"',
+        #     target_string="",
+        #     tail_string=r'"\,"',
+        #     total_page=target_video_source,
+        # )
+        # title = self.get_match_pattern_string(
+        #     head_string=r'text"\:"',
+        #     target_string="",
+        #     tail_string=r'"\}\]',
+        #     total_page=target_video_source,
+        # )
+        # thumbnail_list = self.get_match_pattern_string(
+        #     head_string=r'url"\:"',
+        #     target_string="",
+        #     tail_string=r'"\,"',
+        #     total_page=target_video_source,
+        # )
+        # naver_map_source = requests.get(naver_url, verify=False)
+        # # ISO-8859-1 인코딩 문제로 한글 깨지는 문제 해결
+        # naver_map_source.encoding = "utf-8"
+        # naver_map_source = naver_map_source.text
+        # store_info = self.get_store_info(naver_map_source=naver_map_source)
+        #
+        # return {
+        #     "video_id": video_id[0],
+        #     "playlist_title": title[0],
+        #     "thumbnail_list": thumbnail_list,
+        #     "naver_url": naver_url,
+        #     **store_info,
+        # }
+        pass
 
     def get_store_info(self, naver_map_source: str):
-        pattern = '{target}="([^"]+)"'
-        name = re.search(
-            pattern.format(target=self.naver_url_shop_title_pattern), naver_map_source
-        ).group(1)
-        location = re.search(
-            pattern.format(target=self.naver_url_shop_location_pattern),
-            naver_map_source,
-        ).group(1)
-
-        return {
-            "shop_name": name,
-            "shop_location": location,
-        }
+        pass
+        # pattern = '{target}="([^"]+)"'
+        # name = re.search(
+        #     pattern.format(target=self.naver_url_shop_title_pattern), naver_map_source
+        # ).group(1)
+        # location = re.search(
+        #     pattern.format(target=self.naver_url_shop_location_pattern),
+        #     naver_map_source,
+        # ).group(1)
+        #
+        # return {
+        #     "shop_name": name,
+        #     "shop_location": location,
+        # }
