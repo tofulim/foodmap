6577f6bcb52e8feb141ef2fee1800595cdc3e0a5
vail131 <vail131@naver.com>
2023-11-11 13:40:45 +0900

    [#5] fix parser with naver url parsing and add TODO

--- crawlers/parsers/base_parser.py
+++ crawlers/parsers/base_parser.py
@@ -1,6 +1,7 @@
 import re
 from abc import ABC, abstractmethod
 
+import requests
 from bs4 import BeautifulSoup
 
 
@@ -16,10 +17,10 @@ class AbstractParser(ABC):
     def get_naver_url(
         total_page: str,
     ):
-        pattern = "https?://naver.me/[^-\\\]+"
-        total_pattern = re.search(pattern, total_page).group()
+        pattern = "https?://naver.me/([a-zA-Z0-9]+?)\\\\n"
+        naver_urls = re.findall(pattern, total_page)
 
-        return total_pattern
+        return naver_urls
 
     @staticmethod
     def get_match_pattern_string(
@@ -53,71 +54,58 @@ class AbstractParser(ABC):
 
 class NaverMapParser(AbstractParser):
     """
-    네이버 맵 url 내에 있는 가게 주소와 상호명을 가져온다
+    네이버 맵 url 내에 있는 가게 주소와 상호명을 가져온다.
+    (+ naver map id)
     """
 
     def __init__(self):
         # 더보기란에서 정보를 얻겠다는 것
-        self.type = "more_info"
         self.request_url = "https://www.youtube.com/watch?v={video_id}"
 
     # TODO: scraper로 분리해서 thumnail이랑 더보기란이나 댓글을 key:value로 구분해서 key: html_Source로 하게 해야함
     def parse(self, video_id: str, **kwargs):
+        """
+        개별 url의 영상을 분석해
 
-        # response = requests.get(
-        #     url=self.request_url.format(video_id=video_id), verify=False
-        # )
-        # html_source = BeautifulSoup(response.text, "html.parser")
-        #
-        # html_source.find()
-        # naver_url = self.get_naver_url(
-        #     total_page=target_video_source,
-        # )
-        # video_id = self.get_match_pattern_string(
-        #     head_string=r'videoId"\:"',
-        #     target_string="",
-        #     tail_string=r'"\,"',
-        #     total_page=target_video_source,
-        # )
-        # title = self.get_match_pattern_string(
-        #     head_string=r'text"\:"',
-        #     target_string="",
-        #     tail_string=r'"\}\]',
-        #     total_page=target_video_source,
-        # )
-        # thumbnail_list = self.get_match_pattern_string(
-        #     head_string=r'url"\:"',
-        #     target_string="",
-        #     tail_string=r'"\,"',
-        #     total_page=target_video_source,
-        # )
-        # naver_map_source = requests.get(naver_url, verify=False)
-        # # ISO-8859-1 인코딩 문제로 한글 깨지는 문제 해결
-        # naver_map_source.encoding = "utf-8"
-        # naver_map_source = naver_map_source.text
-        # store_info = self.get_store_info(naver_map_source=naver_map_source)
-        #
-        # return {
-        #     "video_id": video_id[0],
-        #     "playlist_title": title[0],
-        #     "thumbnail_list": thumbnail_list,
-        #     "naver_url": naver_url,
-        #     **store_info,
-        # }
-        pass
+        Args:
+            video_id ():
+            **kwargs ():
+
+        Returns:
+
+        """
+
+        response = requests.get(
+            url=self.request_url.format(video_id=video_id), verify=False
+        )
+        html_source = response.text
+        naver_urls = self.get_naver_url(
+            total_page=html_source,
+        )
+        # TODO: url이 복수일 수 있음. 상점 또한 복수. db schema 정의 필요
+        naver_map_source = requests.get(naver_urls, verify=False)
+        # ISO-8859-1 인코딩 문제로 한글 깨지는 문제 해결
+        naver_map_source.encoding = "utf-8"
+        naver_map_source = naver_map_source.text
+        store_info = self.get_store_info(naver_map_source=naver_map_source)
+
+        return {
+            "video_id": video_id[0],
+            "naver_urls": naver_urls,
+            **store_info,
+        }
 
     def get_store_info(self, naver_map_source: str):
-        pass
-        # pattern = '{target}="([^"]+)"'
-        # name = re.search(
-        #     pattern.format(target=self.naver_url_shop_title_pattern), naver_map_source
-        # ).group(1)
-        # location = re.search(
-        #     pattern.format(target=self.naver_url_shop_location_pattern),
-        #     naver_map_source,
-        # ).group(1)
-        #
-        # return {
-        #     "shop_name": name,
-        #     "shop_location": location,
-        # }
+        pattern = '{target}="([^"]+)"'
+        name = re.search(
+            pattern.format(target=self.naver_url_shop_title_pattern), naver_map_source
+        ).group(1)
+        location = re.search(
+            pattern.format(target=self.naver_url_shop_location_pattern),
+            naver_map_source,
+        ).group(1)
+
+        return {
+            "shop_name": name,
+            "shop_location": location,
+        }
