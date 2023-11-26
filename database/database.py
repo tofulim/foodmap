8ce22192a87f4ced5a1133f7a495f8e20d635975
vail131 <vail131@naver.com>
2023-11-26 10:03:23 +0900

    [#12] dummy commit

---
+++ database/database.py
@@ -0,0 +1,15 @@
+"""
+Author: zed.ai
+Reviewer:
+2023.11.26
+"""
+
+
+class Database:
+    """
+    Firstore로 DB를 구성하고 nosql 형태의 db를 다룬다.
+    주로 크롤링한 데이터를 db에 업로드하는 과정을 처리한다.
+    """
+
+    def __init__(self):
+        pass
